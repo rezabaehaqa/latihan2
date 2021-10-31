ddf7e1dce2725fde3676609e8c3663e6b7e15308
rezabaehaqa <rezabaehaqa22@gmail.com>
2021-10-31 11:05:52 +0700

    menambahkan file luas_keliling_lingkaran.py

---
+++ luas_keliling_lingkaran.py
@@ -0,0 +1,9 @@
+print("menghitung luas dan keliling lingkaran")
+r = int(input("Masukan Jari-jari = "))
+
+phi = 22/7
+luas = phi*r*r
+kel = 2*phi*r
+
+print("luas lingkaran Adalah        =", luas)
+print("keliling lingkaran adalah    =", kel)
